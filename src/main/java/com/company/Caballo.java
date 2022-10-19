67023f0f311a3dbf2e210cc6ecdb55dba21c54d2
Gabriel Pelegri <gpelerii@myuax.com>
2022-10-19 11:35:22 +0200

    Añadido panel

--- src/main/java/com/company/Caballo.java
+++ src/main/java/com/company/Caballo.java
@@ -1,23 +1,24 @@
 package com.company;
 
-import java.awt.TextArea;
-import java.util.Scanner;
 import javax.swing.*;
+import java.util.Scanner;
 
 public class Caballo {
-    static int numCarreras; //Cuántos corredores voy a tener
-    private static OperacionHilos corredor[]; //Array de carreras
+
+    static int numCarreras;
+    private static OperacionHilos corredor[];
     private static int numCorredores;
+
     public static void Lanzar(Scanner teclado) {
 
-        System.out.println("¿Cuántos caballos van a correr?");
-        numCorredores = teclado.nextInt();
-        numCarreras=numCorredores;
+        String corredores = JOptionPane.showInputDialog(null, "¿Cuántos Caballos?");
+        numCorredores = Integer.parseInt(corredores);
+        numCarreras = numCorredores;
         Resultados res = new Resultados(numCorredores, numCarreras);
         corredor = new OperacionHilos[numCarreras];
 
         for (int i = 0; i < 1; i++) {
-            corredor[i] = new OperacionHilos (i+1, res, numCarreras);
+            corredor[i] = new OperacionHilos(i + 1, res, numCarreras);
             corredor[i].start(); //Empiezo el hilo
             try {
                 corredor[i].join();
@@ -26,7 +27,6 @@ public class Caballo {
             }
         }
 
-        //Muestra los resultados de cada carrera, a ver qué corredor tarda menos sprints en llegar a la meta
         System.out.println("Las carreras han finalizado... Han llegado los " + numCarreras + " corredores");
         res.getResultados();
         for (int t = 0; t < numCarreras; t++) {
